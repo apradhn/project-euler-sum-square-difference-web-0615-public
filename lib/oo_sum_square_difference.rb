1e27b6e37d5d7ac8ab0494e82a5ff7bf82e74054
apradhn <abhijit.pradhan6@gmail.com>
2015-07-13 09:26:05 -0400

    all done

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,26 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+  attr_reader :num
+  def initialize(num)
+    @num = num
+  end
+
+def difference
+  sum_of_integers ** 2 - sum_of_squares
+end
+
+def sum_of_squares
+  sum = 0
+  1.upto(num) do |i|
+    sum += i**2
+  end
+  sum
+end
+
+def sum_of_integers
+  sum = 0
+  1.upto(num) do |i|
+    sum += i
+  end
+  sum
+end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,19 @@
-# Implement your procedural solution here!
+def sum_square_difference(num)
+  sum_of_integers(num) ** 2 - sum_of_squares(num)
+end
+
+def sum_of_squares(num)
+  sum = 0
+  1.upto(num) do |i|
+    sum += i**2
+  end
+  sum
+end
+
+def sum_of_integers(num)
+  sum = 0
+  1.upto(num) do |i|
+    sum += i
+  end
+  sum
+end
